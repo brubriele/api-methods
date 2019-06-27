6d417b1eff89786d12dbddeaf2e0b4c95deacb46
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-06-27 17:36:45 -0300

    master - find by primary key

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,17 @@ const Users = models.Users;
 router.get("/", (req, res) => {
     Users.findAll()
     .then(users => {
-        const userList = users.map( user => user.dataValues);
-        res.send(userList)
+        // const userList = users.map( user => user.dataValues.id == req.params.usersId ? user.dataValues : false );
+        res.send(users)
     });
     
 });
+
+router.get("/:id", (req, res) => {
+   Users.findByPk(req.params.id)
+   .then(user => res.send(user))    
+});
+
+
 
 module.exports = router;
