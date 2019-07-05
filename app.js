2fc71c728543b4ec9dbf87bc80bc689e7db1f448
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-05 14:28:28 -0300

    destroy - inicia correção bug delete

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.listen(3842, () => {
     console.log('iniciou!')
 });
 
+app.use(express.json)
 app.use("/user", require("./routes/user"));
 app.use("/productName", require("./routes/product"));
 // app.delete("/Destroy", require("./routes/product"));
