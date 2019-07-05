9f4dd1275bdcfacee0fe1ffa0c9cecf509f2eeb3
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-05 15:55:07 -0300

    code-review - cria rota GET para orders

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-//Associar app.js e models
 const db = require('./models/index');
 const models = require('./models');
 const Products = models.Products;
 
 
 app.listen(3842, () => {
-    console.log('iniciou!')
+  console.log('iniciou!')
 });
 
-app.use(express.json)
+// GET
 app.use("/user", require("./routes/user"));
 app.use("/productName", require("./routes/product"));
-// app.delete("/Destroy", require("./routes/product"));
+app.use("/orders", require("./routes/order"));
 
-// app.delete('/models/products', products.delete);
-app.delete( "/productName/:id", (req, res) =>
-    Products.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then( (result) => res.json(result) )
-  );
+
+// DELETE
+app.delete("/productName/:id", (req, res) =>
+  Products.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((result) => res.json(result))
+);
+
+app.delete("/user/:id", (req, res) =>
+  Products.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((result) => res.json(result))
+);
+
+// POST
+
 
 db.sequelize.sync();
--- routes/product.js
+++ routes/product.js
@@ -5,15 +5,9 @@ const Products = models.Products;
 router.get("/", (req, res) => {
     Products.findAll()
         .then(product => {
-            // const userList = users.map( user => user.dataValues.id == req.params.usersId ? user.dataValues : false );
             res.send(product)
         });
 
-});
-
-router.get("/:productName", (req, res) => {
-    Products.findOne(req.params.productName)
-        .then(product => res.send(product))
 });
 
 router.delete('/post/:id', (req, res) => {
