a6da81181884466ace45933db2edac5a3ffe78af
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-05 17:39:02 -0300

    code-review - implementa rota POST com userID

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 const models = require('../models');
 const Orders = models.Orders;
+const OrderProducts = models.OrderProducts;
 
 router.get("/", (req, res) => {
     Orders.findAll()
+        .then(order => {
+            res.send(order)
+        });
+
+});
+
+router.get("/orders/:id", (req, res) => {
+    Orders.findByPk(req.params.id, {include:[Users, OrderProducts]})
         .then(order => {
             res.send(order)
         });
